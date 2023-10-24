97f08444da9c70708d837c39b6ba59e3578a8e7c
Andrew On <a_on93@hotmail.com>
2023-10-24 09:50:32 -0400

    refactored code to fix formatting of webpage

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -21,62 +21,89 @@ const data = [
     "user": {
       "name": "Descartes",
       "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
+      "handle": "@rd"
+    },
     "content": {
       "text": "Je pense , donc je suis"
     },
     "created_at": 1461113959088
   }
 ]
-$(document).ready(function() {
-const renderTweets = function (tweetsArray) {
-  for (let tweet of tweetsArray) {
-    $('#tweet-container').append(createTweetElement(tweet));
+
+$(document).ready(function () {
+  const createTweetElement = function (tweetObject) {
+    const newTweet = ` <article class= "tweet"> 
+    <header>
+      <div>
+        <img src = ${tweetObject.user.avatars}/>
+        <span> ${tweetObject.user.name} </span>
+      </div>
+      <p>${tweetObject.user.handle}</p>
+    </header>
+      <div>
+        <p>${tweetObject.content.text}</p>
+      </div>
+    <footer>
+      <p class="tweet-date">${timeago.format(tweetObject.created_at)}</p>
+      <div> 
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>  
+    </footer>
+    </article>
+    `
+    return newTweet;
+  };
+
+  const renderTweets = function (tweetsArray) {
+    for (let tweet of tweetsArray) {
+      $('#tweets-container').append(createTweetElement(tweet));
+    }
   }
-}
-const createTweetElement = function (tweetObject) {
-  const newTweet = ` <article> 
-  <header class = "newtweetHead">
-    <div>
-      <img src = ${tweetObject.user.avatars}/>
-    </div>
-    <div>
-      <span> ${tweetObject.user.name} </span>
-    <div>
-      <span>${tweetObject.user.handle}</span>
-    </div>
-  </header>
-    <div>
-      <span>${tweetObject.content.text}</span>
-    </div>
-  <footer class = "newTweetFoot">
-    <div>
-      <span>${tweetObject.created_at}</span>
-      <span class="tweet-date">${timeago.format(tweetObject.created_at)}</span>
-    </div>
-    <div> 
-      <span> <i class="fas fa-flag"> </i> <i class="fas fa-retweet"> </i> <i class="fas fa-heart"> </i> </span>
-    </div>  
-  </footer>
-  </article>
-  `
-  return newTweet;
-};
 
-renderTweets(data);
-const loadTweets = function () {
-  $.ajax('/tweets', { method: 'GET' })
-  .then(function(tweets) {
-    renderTweets(tweets);
-  });
- };
+  const loadTweets = function () {
+    $.ajax('/tweets', {
+      method: 'GET',
+      dataType: 'JSON',
+      success: tweets => renderTweets(tweets),
+      error: (error) => console.error(error)
+    });
+  };
 
- loadTweets();
- 
-const submitTweet = $('#submitForm');
-submitTweet.on('submit', function(event) {
-  const data = $('#submitForm').serialize();
-  event.preventDefault();
-  $.ajax(data, {method: 'POST'})
+  const errorMessage = function (message) {
+    $('#submit-tweet').prepend(
+      $("<span class='error'>")
+        .text(message)
+    )
+  };
+
+  $('#submitForm').on('submit', function (event) {
+    event.preventDefault();
+
+    const userInput = $('#tweet-text').val();
+
+    if (userInput === '' || null) {
+      errorMessage('You cannot post a blank tweet');
+      return;
+    }
+
+    if (userInput.length > 140) {
+      errorMessage('Please make sure your tweet is under 140 characters!');
+      return;
+    }
+
+    $.ajax('/tweets', {
+      data: $(this).serialize(),
+      method: 'POST',
+      success: () => {
+        loadTweets();
+        $textArea.val(''); // clear textarea
+        $('.counter').text('140'); // reset counter to 140
+      },
+      error: (error) => console.error(error)
+    });
   });
+
+  loadTweets();
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  $('textarea').keyup(function() {
+  $('#tweet-text').keyup(function() {
     const tweetLimit = 140;
     let currentInputLength = $(this).val().length;
     let charsRemaining = tweetLimit - currentInputLength;
-    $(this).nextAll().text(charsRemaining);
+    $(this).next().find('.counter').text(charsRemaining);
     if (charsRemaining < 0) {
-      $(this).nextAll().css("color", "red");
+      $(this).next().find('.counter').css("color", "red");
     }
   });
 });
