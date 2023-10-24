44dd23bf67923fd21e870912aab493a94ee025d4
Andrew On <a_on93@hotmail.com>
2023-10-24 09:51:31 -0400

    add error handling

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -107,3 +107,9 @@ $(document).ready(function () {
 
   loadTweets();
 });
+
+const escape = function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
